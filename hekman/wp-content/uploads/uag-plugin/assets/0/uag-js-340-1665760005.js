669e0edbfd5c98338361196e939b1473c357d734
Johan <johangeel1996@gmail.com>
2022-10-15 00:56:34 +0200

    added hekman

---
+++ hekman/wp-content/uploads/uag-plugin/assets/0/uag-js-340-1665760005.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"09b58f2c-9395-4b3f-935d-413fa0735132","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-09b58f2c-9395-4b3f-935d-413fa0735132' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"09b58f2c-9395-4b3f-935d-413fa0735132","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-09b58f2c-9395-4b3f-935d-413fa0735132' );
+});
+ })
